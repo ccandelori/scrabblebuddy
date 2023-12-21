c5f14f0f3623eee0978c1cf14b84902f12c0b2bd
ccandelori <cameron.nigro@gmail.com>
2023-12-21 13:53:00 -0500

    Initial Commit

---
+++ ScrabbleBuddy/ScrabbleBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrabbleBuddyApp.swift
+//  ScrabbleBuddy
+//
+//  Created by Cameron Candelori on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrabbleBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
